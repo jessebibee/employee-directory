dc1f9b318f91e32a01c74361478cf458af366e3e
Jesse Bibee <jesse.bibee@gmail.com>
2014-10-11 23:15:52 -0700

    location and phone filters to format values for views

---
+++ EmployeeDirectory.Web/app/js/locationFilter.js
@@ -0,0 +1,15 @@
+﻿(function (app) {
+    'use strict';
+
+    app.filter('location', ['$filter', function ($filter) {
+        var locations = {
+            1: 'Austin',
+            2: 'Dallas',
+            3: 'Houston'
+        };
+
+        return function (input) {
+            return locations[input] || '';
+        };
+    }]);
+})(angular.module('app'));
---
+++ EmployeeDirectory.Web/app/js/phoneFilter.js
@@ -0,0 +1,15 @@
+﻿(function (app) {
+    'use strict';
+
+    app.filter('phone', ['$filter', function ($filter) {
+        return function (input) {
+            if (input && input.length === 10) {
+                var areaCode = input.substring(0, 3);
+                var prefix = input.substring(3, 6);
+                var number = input.substring(6, 10);
+                return '(' + areaCode + ') ' + prefix + '-' + number;
+            }
+            return input;
+        };
+    }]);
+})(angular.module('app'));
